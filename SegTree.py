9f634d4fe1afb4e58f2f8e6abb418d7c1431bdd9
lucys1 <slucy6814@gmail.com>
2018-01-19 21:49:03 -0500

    format;unit test;2,3,4-sum

--- SegTree.py
+++ SegTree.py
@@ -1,12 +1,14 @@
+import unittest
+
 class SegTree(object):
 
-	#start incl, end excl
+	# start incl, end excl
 	def __init__(self, lst, start=0, end=None):
 		if end is None:
-			end=len(lst)
+			end = len(lst)
 		self.start = start
 		self.end = end
-		if start == end-1:
+		if start == end - 1:
 			self.value = lst[start]
 			self.lch = None
 			self.rch = None
@@ -18,9 +20,9 @@ class SegTree(object):
 			self.value = self.operation(self.lch.value, self.rch.value)
 
 	def modify(self, idx, new):
-		if idx<self.start or idx>=self.end:
+		if idx < self.start or idx >= self.end:
 			return
-		elif idx==self.start and idx+1==self.end:
+		elif idx == self.start and idx + 1 == self.end:
 			self.value = new
 		else:
 			self.rch.modify(idx, new)
@@ -28,41 +30,46 @@ class SegTree(object):
 			self.value = self.operation(self.lch.value, self.rch.value)
 
 	def query(self, start, end):
-		if start<=self.start and end>=self.end:
+		if start <= self.start and end >= self.end:
 			return self.value
-		if end<=self.start and start>=self.end:
+		if end <= self.start or start >= self.end:
 			return 0
 		mid = self.lch.end
-		s=0
+		s = 0
 		if start < mid:
-			s+=self.lch.query(start, end)
+			s += self.lch.query(start, end)
 		if end > mid:
-			s+=self.rch.query(start, end)
+			s += self.rch.query(start, end)
 		return s
 
 	def operation(self, x, y):
-		return x+y
+		return x + y
 
 
 # test case from hell
 from random import randint
-l=[]
-for i in range(20):
-	l.append(randint(-100, 100))
-tree = SegTree(l)
-for _ in range(30):
-	j = randint(0, len(l)-1)
-	k = randint(0, len(l)-1)
-	if k<j:
-		j,k=k,j
-	exp = 0
-	for m in range(j,k):
-		exp += l[m]
-	assert exp == tree.query(j,k)
 
-	j = randint(0, len(l)-1)
-	k = randint(-100, 100)
-	l[j] = k
-	tree.modify(j, k)
+class Test(unittest.TestCase):
+	def test(self):
+		l = []
+		for i in range(20):
+			l.append(randint(-100, 100))
+		tree = SegTree(l)
+		for _ in range(30):
+			j = randint(0, len(l) - 1)
+			k = randint(0, len(l) - 1)
+			if k < j:
+				j, k = k, j
+			exp = 0
+			for m in range(j, k):
+				exp += l[m]
+			self.assertEqual(tree.query(j, k), exp)
 
-print "ok"
+			j = randint(0, len(l) - 1)
+			k = randint(-100, 100)
+			l[j] = k
+			tree.modify(j, k)
+
+if __name__ == '__main__':
+    unittest.main()
+
--- Trie.py
+++ Trie.py
@@ -1,52 +1,59 @@
-charCount=26
+import unittest
+
+charCount = 26
 
 class Trie(object):
 	def __init__(self):
-		self.count=0
-		self.children=0
-		self.lst=[None]*charCount
+		self.count = 0
+		self.children = 0
+		self.lst = [None] * charCount
 
 	def add(self, string, idx=0):
 		self.children += 1
-		if idx==len(string):
-			self.count+=1
+		if idx == len(string):
+			self.count += 1
 			return
-		i=ord(string[idx])-ord('a')
+		i = ord(string[idx]) - ord('a')
 		if not self.lst[i]:
 			self.lst[i] = Trie()
-		self.lst[i].add(string, idx+1)
+		self.lst[i].add(string, idx + 1)
 
 	def queryCount(self, string, idx=0):
-		if idx==len(string):
+		if idx == len(string):
 			return self.count
-		i=ord(string[idx])-ord('a')
+		i = ord(string[idx]) - ord('a')
 		if not self.lst[i]:
 			return 0
-		return self.lst[i].queryCount(string, idx+1)
+		return self.lst[i].queryCount(string, idx + 1)
 
 	def queryChildren(self, string, idx=0):
-		if idx==len(string):
+		if idx == len(string):
 			return self.children
-		i=ord(string[idx])-ord('a')
+		i = ord(string[idx]) - ord('a')
 		if not self.lst[i]:
 			return 0
-		return self.lst[i].queryChildren(string, idx+1)
+		return self.lst[i].queryChildren(string, idx + 1)
 
 	def exist(self, string):
 		return self.query(self, string) > 0
 
-#test cases
-string = ["jkdhfaksjfk", "asfdjkasjfd", "askdjfha", "asdfwera"]
-root=Trie()
-for s in string:
-	for i in range(len(s)):
-		root.add(s[i:])
 
-assert root.queryCount("jfk") == 1
-assert root.queryCount("aks") == 0
-assert root.queryCount("a") == 2
-assert root.queryChildren("a") == 7
-assert root.queryChildren("mn") == 0
-assert root.queryChildren("jf") == 3
+# test cases
+class Test(unittest.TestCase):
+	def test(self):
+		string = ["jkdhfaksjfk", "asfdjkasjfd", "askdjfha", "asdfwera"]
+		root = Trie()
+		for s in string:
+			for i in range(len(s)):
+				root.add(s[i:])
 
-print "ok"
+		self.assertEqual(root.queryCount("jfk"), 1)
+		self.assertEqual(root.queryCount("aks"), 0)
+		self.assertEqual(root.queryCount("a"), 2)
+		self.assertEqual(root.queryChildren("a"), 7)
+		self.assertEqual(root.queryChildren("mn"), 0)
+		self.assertEqual(root.queryChildren("jf"), 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
