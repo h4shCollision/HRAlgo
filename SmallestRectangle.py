ca8686f95df264c063aaf818c0fefce9f80626d7
lucys1 <slucy6814@gmail.com>
2018-06-17 21:44:07 -0400

    improvement on smallest rectangle problem

--- SmallestRectangle.py
+++ SmallestRectangle.py
@@ -4,6 +4,7 @@
 # assume no 2 points overlap
 # return -1 if no rectangle is possible
 # O(n^2) time (unless I screwed up) assuming const time dictionary insert/lookup
+# coordinates from lower left corner to upper right corner (x1, y1, x2, y2)
 
 from collections import defaultdict
 import unittest
@@ -16,41 +17,32 @@ def smallestRectangle(pts):
 	recMap = {}
 	mnArea = -1
 	coords = None  # blx, bly, w, h
-	for y, xs in axisMap.iteritems():
+	for y, xs in sorted(axisMap.iteritems()): # O(n log n) to sort
 		for i in range(len(xs)):
 			pt1 = xs[i]
-			for j in range(i + 1, len(xs)):  # execute this loop at most O(n^2) times
+			for j in range(i + 1, len(xs)):  # since (pt1, y), (pt2, y) are distinct pts of the input set, execute this
+											# loop at most O(n^2) times
 				pt2 = xs[j]
 				# (minx, dx)
 				key = (min(pt1, pt2), abs(pt1 - pt2))
 				if key not in recMap:
-					recMap[key] = [y]
+					recMap[key] = y
 				else:
-					ys = recMap[key]
-					minh = abs(ys[0] - y)
-					othery = ys[0]
-					for k in range(1, len(ys)):  # another O(n) -> more analysis later
-						h = abs(ys[k] - y)
-						if h < minh:
-							minh = h
-							othery = ys[k]
-					area = minh * key[1]
-					if mnArea == -1 or area < mnArea:
+					othery = recMap[key]
+					area = (y - othery) * key[1]
+					if mnArea == -1 or area < mnArea: # change this line if calculating largest rectangle
 						mnArea = area
-						coords = (key[0], min(othery, y), key[1], minh)
-					ys.append(y)
+						coords = (key[0], othery, key[0] + key[1], y)
+					recMap[key] = y # remove this line if calculating largest rectangle
 	return mnArea, coords
 
-# Analysis: when we multiple out the big O notations, it becomes O(n^3), however, process executes the loop starting at
-# line 32(hope this doesn't change) exactly once per possible rectangle, it means # of executions of the innermost loop
-# is capped at O(n^2), so total runtime is O(n^2)
 
 # test cases
 class Test(unittest.TestCase):
 	def test(self):
 		pts = [(-1, -1), (1, -1), (-1, 1), (1, 1),
 			   (2, 2), (2, 1), (1, 2), (-1, 2), (2, -1), (0, 2), (1, 3)]
-		self.assertEquals(smallestRectangle(pts), (1, (1, 1, 1, 1)))
+		self.assertEquals(smallestRectangle(pts), (1, (1, 1, 2, 2)))
 
 	def testNoRec(self):
 		pts = [(-1, -1), (1, -1), (-1, 1), (2, 2), (-1, 2), (0, 2), (1, 3)]
